839d8672f7d45ae7940379a8b3ea147599a74728
Rycer <szymkris5@gmail.com>
2024-08-16 09:28:57 +0200

    input and load pick data

--- main.js
+++ main.js
@@ -1,31 +1,113 @@
-
-const data = {
-  "B1" : {
-    "top" : "bad",
-    "jungle" : "bad",
-    "mid" : "good",
-    "adc" : "good",
-    "support" : "bad",
-    "exceptions" : "lilia"
-  }
+const pick_data = {
+  b1: {
+    top: "maybe",
+    jungle: "bad",
+    mid: "good",
+    adc: "maybe",
+    support: "bad",
+    exceptions: "lilia cait jinx aurora gragas",
+  },
+  b2: {
+    top: "maybe",
+    jungle: "maybe",
+    mid: "good",
+    adc: "good",
+    support: "good",
+    exceptions: "",
+  },
+  b3: {
+    top: "maybe",
+    jungle: "maybe",
+    mid: "good",
+    adc: "good",
+    support: "good",
+    exceptions: "",
+  },
+  b4: {
+    top: "good",
+    jungle: "good",
+    mid: "maybe",
+    adc: "good",
+    support: "good",
+    exceptions: "",
+  },
+  b5: {
+    top: "good",
+    jungle: "good",
+    mid: "maybe",
+    adc: "good",
+    support: "good",
+    exceptions: "",
+  },
+  r1: {
+    top: "maybe",
+    jungle: "bad",
+    mid: "good",
+    adc: "maybe",
+    support: "maybe",
+    exceptions: "",
+  },
+  r2: {
+    top: "maybe",
+    jungle: "bad",
+    mid: "good",
+    adc: "maybe",
+    support: "maybe",
+    exceptions: "",
+  },
+  r3: {
+    top: "good",
+    jungle: "maybe",
+    mid: "maybe",
+    adc: "maybe",
+    support: "good",
+    exceptions: "",
+  },
+  r4: {
+    top: "bad",
+    jungle: "good",
+    mid: "good",
+    adc: "good",
+    support: "good",
+    exceptions: "",
+  },
+  r5: {
+    top: "good",
+    jungle: "good",
+    mid: "maybe",
+    adc: "good",
+    support: "good",
+    exceptions: "",
+  },
 };
 
 const b1 = document.getElementById("b-one");
-b1.innerText = "Can pick:"
-const b1_data = data["B1"];
-let i = 5;
-if(b1_data["top"] == "good"){
-  b1.innerText += " toplane"
-}
-if(b1_data["jungle"] == "good"){
-  b1.innerText += " jungle"
-}
-if(b1_data["mid"] == "good"){
-  b1.innerText += " midlane"
-}
-if(b1_data["adc"] == "good"){
-  b1.innerText += " adc"
-}
-if(b1_data["support"] == "good"){
-  b1.innerText += " support"
+const b2 = document.getElementById("b-two");
+const b3 = document.getElementById("b-three");
+const b4 = document.getElementById("b-four");
+const b5 = document.getElementById("b-five");
+const r1 = document.getElementById("r-one");
+const r2 = document.getElementById("r-two");
+const r3 = document.getElementById("r-three");
+const r4 = document.getElementById("r-four");
+const r5 = document.getElementById("r-five");
+const picks = [b1, b2, b3, b4, b5, r1, r2, r3, r4, r5];
+const pickstrings = [
+  "b1",
+  "b2",
+  "b3",
+  "b4",
+  "b5",
+  "r1",
+  "r2",
+  "r3",
+  "r4",
+  "r5",
+];
+const roles = ["top", "jungle", "mid", "adc", "support", "exceptions"];
+for (let j = 0; j < picks.length; j++) {
+  for (let i = 0; i < roles.length; i++) {
+    picks[j].innerText +=
+      roles[i] + " " + pick_data[pickstrings[j]][roles[i]] + ", ";
+  }
 }
