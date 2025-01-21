c06f2a3a7290c020f737d89e6692f6f1f7cb0cc3
aCertainProgrammer <a_certain_programmer@proton.me>
2025-01-21 18:06:41 +0100

    revert the last change

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ import { Backend } from "./backend.js";
 import { Controller } from "./controller.js";
 import { Scraper } from "./scraper.js";
 import { UserInterface } from "./userinterface.js";
-import ZeroMd, { STYLES } from "../node_modules/zero-md/dist/index.js";
+import ZeroMd, { STYLES } from "https://cdn.jsdelivr.net/npm/zero-md@3";
 
 const controller = new Controller(
 	new Scraper(".champion-pick", ".champion-ban"),
